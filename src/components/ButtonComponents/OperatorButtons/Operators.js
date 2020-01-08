d0ee3889a4ea1b026afb9a492560dd735b4cae05
hoangvu ong(devin) <hoangvu@hoangvus-MacBook-Air.local>
2020-01-07 18:53:27 -0800

    added all displays to app

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,21 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {operators} from "../../../data"
+import OperatorButton from "./OperatorButton"
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorState, setOperator] = useState(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operatorState.map(ele => <OperatorButton char={ele.char} value={ele.value}/>)}
     </div>
   );
 };
+
+export default Operators;
